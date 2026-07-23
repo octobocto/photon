95d61978d6b966ea48c9502aa416d8f353f315fd
Ash Manning <ash@layertwolabs.com>
2026-07-24 01:15:12 +0800

    update to latest bip300301_enforcer

--- lib/build.rs
+++ lib/build.rs
@@ -29,12 +29,17 @@ where
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const COMMON_PROTO: &str = "../proto/proto/cusf/common/v1/common.proto";
+    const COMMON_PROTO: &str =
+        "../bip300301_enforcer/proto/cusf/common/v1/common.proto";
+    const MINING_PROTO: &str =
+        "../bip300301_enforcer/proto/cusf/mainchain/v1/mining.proto";
     const VALIDATOR_PROTO: &str =
-        "../proto/proto/cusf/mainchain/v1/validator.proto";
-    const WALLET_PROTO: &str = "../proto/proto/cusf/mainchain/v1/wallet.proto";
-    const ALL_PROTOS: &[&str] = &[COMMON_PROTO, VALIDATOR_PROTO, WALLET_PROTO];
-    const INCLUDES: &[&str] = &["../proto/proto"];
+        "../bip300301_enforcer/proto/cusf/mainchain/v1/validator.proto";
+    const WALLET_PROTO: &str =
+        "../bip300301_enforcer/proto/cusf/mainchain/v1/wallet.proto";
+    const ALL_PROTOS: &[&str] =
+        &[COMMON_PROTO, MINING_PROTO, VALIDATOR_PROTO, WALLET_PROTO];
+    const INCLUDES: &[&str] = &["../bip300301_enforcer/proto"];
     let file_descriptors = protox::compile(ALL_PROTOS, INCLUDES)?;
     let file_descriptor_path = PathBuf::from(
         env::var("OUT_DIR").expect("OUT_DIR environment variable not set"),
@@ -50,7 +55,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
     let () = compile_protos_with_config(
         &file_descriptor_path,
-        &[VALIDATOR_PROTO, WALLET_PROTO],
+        &[VALIDATOR_PROTO, MINING_PROTO, WALLET_PROTO],
         INCLUDES,
         |config| {
             config
