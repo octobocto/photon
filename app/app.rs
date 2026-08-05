LayerTwo-Labs/thunder-rust

RPC: regenerate utreexo proofs in `submit_transaction`

app/app.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
 
 use fallible_iterator::FallibleIterator as _;
 use futures::{StreamExt, TryFutureExt};
@@ -280,10 +280,11 @@ impl App {
         update(self.node.as_ref(), &mut self.utxos.write(), &self.wallet)
     }
 
-    pub fn submit_transaction(
-        &self,
-        tx: &thunder::types::AuthorizedTransaction,
-    ) -> Result<(), Error> {
+    /// Regenerate proofs and submit transaction
+    pub fn submit_transaction<Tx>(&self, tx: Tx) -> Result<(), Error>
+    where
+        Tx: BorrowMut<thunder::types::AuthorizedTransaction>,
+    {
         self.node.submit_transaction(tx)?;
         let () = self.update()?;
         Ok(())
@@ -291,7 +292,7 @@ impl App {
 
     pub fn sign_and_send(&self, tx: Transaction) -> Result<(), Error> {
         let authorized_transaction = self.wallet.authorize(tx)?;
-        self.submit_transaction(&authorized_transaction)
+        self.submit_transaction(authorized_transaction)
     }
 
     pub async fn get_new_main_address(

app/rpc_server.rs
@@ -332,12 +332,12 @@ impl RpcServer for RpcServerImpl {
         transaction: thunder::types::Transaction,
         broadcast: Option<bool>,
     ) -> RpcResult<thunder::types::AuthorizedTransaction> {
-        let authorized =
+        let mut authorized =
             self.app.wallet.authorize(transaction).map_err(custom_err)?;
         if let Some(true) = broadcast {
             let () = self
                 .app
-                .submit_transaction(&authorized)
+                .submit_transaction(&mut authorized)
                 .map_err(custom_err)?;
         }
         Ok(authorized)
@@ -345,11 +345,11 @@ impl RpcServer for RpcServerImpl {
 
     async fn submit_transaction(
         &self,
-        transaction: thunder::types::AuthorizedTransaction,
+        mut transaction: thunder::types::AuthorizedTransaction,
     ) -> RpcResult<Txid> {
         let () = self
             .app
-            .submit_transaction(&transaction)
+            .submit_transaction(&mut transaction)
             .map_err(custom_err)?;
         Ok(transaction.transaction.txid())
     }

octobocto/photon

app/app.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
 
 use fallible_iterator::FallibleIterator as _;
 use futures::{StreamExt, TryFutureExt};
@@ -310,10 +310,11 @@ impl App {
         update(self.node.as_ref(), &mut self.utxos.write(), &self.wallet)
     }
 
-    pub fn submit_transaction(
-        &self,
-        tx: &photon::types::AuthorizedTransaction,
-    ) -> Result<(), Error> {
+    /// Regenerate proofs and submit transaction
+    pub fn submit_transaction<Tx>(&self, tx: Tx) -> Result<(), Error>
+    where
+        Tx: BorrowMut<photon::types::AuthorizedTransaction>,
+    {
         self.node.submit_transaction(tx)?;
         let () = self.update()?;
         Ok(())
@@ -321,7 +322,7 @@ impl App {
 
     pub fn sign_and_send(&self, tx: Transaction) -> Result<(), Error> {
         let authorized_transaction = self.wallet.authorize(tx)?;
-        self.submit_transaction(&authorized_transaction)
+        self.submit_transaction(authorized_transaction)
     }
 
     pub async fn get_new_main_address(

app/rpc_server.rs
@@ -332,12 +332,12 @@ impl RpcServer for RpcServerImpl {
         transaction: photon::types::Transaction,
         broadcast: Option<bool>,
     ) -> RpcResult<photon::types::AuthorizedTransaction> {
-        let authorized =
+        let mut authorized =
             self.app.wallet.authorize(transaction).map_err(custom_err)?;
         if let Some(true) = broadcast {
             let () = self
                 .app
-                .submit_transaction(&authorized)
+                .submit_transaction(&mut authorized)
                 .map_err(custom_err)?;
         }
         Ok(authorized)
@@ -345,11 +345,11 @@ impl RpcServer for RpcServerImpl {
 
     async fn submit_transaction(
         &self,
-        transaction: photon::types::AuthorizedTransaction,
+        mut transaction: photon::types::AuthorizedTransaction,
     ) -> RpcResult<Txid> {
         let () = self
             .app
-            .submit_transaction(&transaction)
+            .submit_transaction(&mut transaction)
             .map_err(custom_err)?;
         Ok(transaction.transaction.txid())
     }